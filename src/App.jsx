5949924d74561af3dc680563adbb67c0041f95b4
Jacek Jarvis <jacekjarvis@gmail.com>
2025-03-13 13:16:26 -0400

    Add Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    // PLAYERS
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+
+        <h2>GAME BOARD</h2>
+      </div>
+      <h2>Log</h2>
+    </main>
+  );
 }
 
-export default App
+export default App;
