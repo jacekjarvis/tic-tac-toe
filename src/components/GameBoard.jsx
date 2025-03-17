0d92ebb8ae227f6c40030a2cc6c8741298f5abc4
Jacek Jarvis <jacekjarvis@gmail.com>
2025-03-17 15:04:52 -0400

    Create GameBoard and Clickable Buttons

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectedButton(rowIndex, colIndex) {
+    console.log(rowIndex, colIndex);
+    setGameBoard((prevBoard) => {
+      //create copy of the current board
+      const updatedBoard = [...prevBoard.map((row) => [...row])];
+
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => handleSelectedButton(rowIndex, colIndex)}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,39 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  function handlePlayerNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let btnCaption = "Edit";
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    btnCaption = "Save";
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handlePlayerNameChange}
+      />
+    );
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 }
